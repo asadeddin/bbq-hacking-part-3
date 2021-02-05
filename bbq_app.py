a726daa099d345e0c2934a56a8a01fb00ded562a
Ahmad <asadeddin@gmail.com>
2021-02-04 21:49:11 -0800

    Made it work

--- bbq_app.py
+++ bbq_app.py
@@ -3,9 +3,10 @@ import csv
 import datetime
 
 # Connect to the device
-connect_key = bytearray.fromhex("2101020304000000000000") # Update this with correct connection key you obtained in part 2
+connect_key = bytearray.fromhex("2107060504030201b8220000000000") # Update this with correct connection key you obtained in part 2
 enable_notifications = bytearray.fromhex("0b0100000000") # This value is correct, no need to update
-bbq_mac = "ff:ff:ff:ff:ff:ff" # Update this with your BBQ device's MAC address
+bbq_mac = "34:14:b5:47:26:7a" # Update this with your BBQ device's MAC address
+
 
 adapter = pygatt.GATTToolBackend()
 
@@ -23,15 +24,17 @@ def handle_notification(handle, value):
         celcius = int(int.from_bytes(value[i:i+2], "little") / 10)
         f_degrees = fahrenheit(celcius)
         temps[f"Probe-{int(i/2)+1}"] = f_degrees
+    print(temps)
     with open("temperature_log.csv", "a") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow([temps[field] for field in temps])
-    
-        
+
+
 try:
     adapter.start()
 
     try:
+        print("Trying to connect")
         device = adapter.connect(bbq_mac,timeout=20)
     except:
         print("Couldn't connect to the device, retrying...")
@@ -43,7 +46,7 @@ try:
     # Enable notifications by writing to 0x34
     device.char_write_handle(0x0034, enable_notifications)
     print("Connected with the device.")
-    
+
     with open('temperature_log.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Timestamp", "Probe 1", "Probe 2", "Probe 3", "Probe 4"])
@@ -55,12 +58,9 @@ try:
             device.subscribe("0000fff4-0000-1000-8000-00805f9b34fb", callback=handle_notification)
         except:
             pass
-    
+
     input("Enter any key to quit....")
-    
+
 
 finally:
     adapter.stop()
-
-
-
